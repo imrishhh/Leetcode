434c4ea7c8d88c336bd7988cb9a3983e6f5a9603
Rishab Karki <nullrish@gmail.com>
2026-07-16 19:14:11 +0545

    Longest Consecutive

---
+++ Arrays_Hashing/longest_consecutive.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        longest = 0
+        for num in nums:
+            if num - 1 not in nums_set:
+                length = 1
+                while num + length in nums_set:
+                    length += 1
+                longest = max(longest, length)
+        return longest
+
+
+nums = [20, 4, 7, 8, 9, 10, 1, 2, 30]
+sol = Solution()
+print(sol.longestConsecutive(nums))
